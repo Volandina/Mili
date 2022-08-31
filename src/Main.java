dd33e23f99cf858a1aa9528d033124c253755546
Елена Кливер <volandmart@mail.ru>
2022-08-31 16:14:21 +0500

    first

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int stoimost = 1000;
+        int x = 20;
+        int mili = stoimost/x;
+        System.out.println("Начисленно мили " + mili);
+
+        // Рассчитываете количество бонусных милль, используя
+        // значения заведённых переменных. Ответ сохраняете в
+        // новую переменную и выводите на экран
+    }
+}
