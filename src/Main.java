f396912a33a9d86e52b0b136427c9db19f2629d5
Елена Кливер <volandmart@mail.ru>
2022-09-04 18:54:17 +0500

    variable edit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         int stoimost = 1000;
-        int x = 20;
-        int mili = stoimost/x;
+        int stepOfRubles = 20;
+        int mili = stoimost / stepOfRubles;
         System.out.println("Начисленно мили " + mili);
 
     }
